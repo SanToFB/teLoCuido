a194fea02bca2f33c7bf93bd790bc7af918d7419
santiago fernandez <sanfernz_88@hotmail.com>
2023-02-27 18:06:55 -0300

    index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 
-router.get('/teLoCuido', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'teLoCuido' });
 });
 
